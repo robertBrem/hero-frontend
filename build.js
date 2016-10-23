ca9d30cacf0f19aebe43f1084ddc34fc9af91f2d
robertBrem <brem_robert@hotmail.com>
2016-10-23 15:27:45 +0200

    initial commit

--- build.js
+++ build.js
@@ -6,10 +6,12 @@ var build = "docker build -t " + imageName + " .";
 $EXEC(build);
 print($OUT);
 print($ERR);
+
 var dockerLogin = "docker login --username=" + $ENV.USER_NAME + " --password=" + $ENV.PASSWORD;
 $EXEC(dockerLogin);
 print($OUT);
 print($ERR);
+
 var push = "docker push " + imageName;
 $EXEC(push);
 print($OUT);
